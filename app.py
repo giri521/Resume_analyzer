c3f2ad7a3332ebd425d77d53cb865538aae66693
VENNAPUSA GIRIVARDHAN REDDY <135786118+giri521@users.noreply.github.com>
2025-06-16 18:37:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,5 +85,6 @@ def upload_resume():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+# To:
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
